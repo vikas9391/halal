6761ebf8c4e87097bbb594e8b906b007410852c8
Gummadala vikas <vikas93912@gmail.com>
2026-08-21 21:27:34 +0530

    Add staff destination CRUD API

--- backend/destinations/views.py
+++ backend/destinations/views.py
@@ -1,17 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from rest_framework.permissions import AllowAny
 
 from .models import Destination
 from .serializers import DestinationSerializer
 
 
-class DestinationViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    GET /api/v1/destinations/
-    GET /api/v1/destinations/<slug>/
-    """
-
+class DestinationViewSet(viewsets.ModelViewSet):
     queryset = Destination.objects.all()
     serializer_class = DestinationSerializer
-    permission_classes = [AllowAny]
     lookup_field = "slug"
+
+    def get_permissions(self):
+        if self.action in ("list", "retrieve"):
+            return [AllowAny()]
+        return [permissions.IsAdminUser()]
