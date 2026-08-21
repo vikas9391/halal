37b47bb760ee2dc05150122defa1fb17047c94ed
Gummadala vikas <vikas93912@gmail.com>
2026-08-21 20:42:54 +0530

    Make traveler compliance migration safe for existing bookings

--- backend/bookings/migrations/0002_booking_payment_fields_traveler_documents.py
+++ backend/bookings/migrations/0002_booking_payment_fields_traveler_documents.py
@@ -1,49 +1,12 @@
 from django.db import migrations, models
 
-
 class Migration(migrations.Migration):
     dependencies = [("bookings", "0001_initial")]
-
     operations = [
-        migrations.AddField(
-            model_name="booking",
-            name="payment_type",
-            field=models.CharField(
-                choices=[("full_payment", "Full Payment"), ("down_payment", "Down Payment ONLY")],
-                default="full_payment",
-                max_length=20,
-            ),
-        ),
-        migrations.AddField(
-            model_name="booking",
-            name="payment_method",
-            field=models.CharField(
-                choices=[("card", "Credit/Debit Card"), ("zelle", "Zelle")],
-                default="card",
-                max_length=10,
-            ),
-        ),
-        migrations.AddField(
-            model_name="traveler",
-            name="passport_status",
-            field=models.CharField(choices=[("valid", "Valid"), ("expired", "Expired")], max_length=8),
-        ),
-        migrations.AddField(
-            model_name="traveler",
-            name="mobility_assistance",
-            field=models.CharField(
-                choices=[("yes", "Yes"), ("wheelchair_assistance", "Need help for pushing wheelchair")],
-                max_length=24,
-            ),
-        ),
-        migrations.AddField(
-            model_name="traveler",
-            name="passport_document",
-            field=models.FileField(upload_to="private/passports/%Y/%m/"),
-        ),
-        migrations.AddField(
-            model_name="traveler",
-            name="passport_photo",
-            field=models.ImageField(upload_to="private/passport-photos/%Y/%m/"),
-        ),
+        migrations.AddField(model_name="booking", name="payment_type", field=models.CharField(choices=[("full_payment", "Full Payment"), ("down_payment", "Down Payment ONLY")], default="full_payment", max_length=20)),
+        migrations.AddField(model_name="booking", name="payment_method", field=models.CharField(choices=[("card", "Credit/Debit Card"), ("zelle", "Zelle")], default="card", max_length=10)),
+        migrations.AddField(model_name="traveler", name="passport_status", field=models.CharField(choices=[("valid", "Valid"), ("expired", "Expired")], default="valid", max_length=8)),
+        migrations.AddField(model_name="traveler", name="mobility_assistance", field=models.CharField(choices=[("yes", "Yes"), ("wheelchair_assistance", "Need help for pushing wheelchair")], default="yes", max_length=24)),
+        migrations.AddField(model_name="traveler", name="passport_document", field=models.FileField(blank=True, null=True, upload_to="private/passports/%Y/%m/")),
+        migrations.AddField(model_name="traveler", name="passport_photo", field=models.ImageField(blank=True, null=True, upload_to="private/passport-photos/%Y/%m/")),
     ]
